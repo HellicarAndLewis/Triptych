4fff3d420861b29cf98d7a9403a674eed8668424
Marek Bereza <bereza@gmail.com>
2012-06-29 15:36:57 +0100

    weird video depth echo experiment

---
+++ KinectVideoDelayThing/src/testApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxKinect.h"
+
+class testApp : public ofBaseApp{
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofxKinect kinect;
+	deque<unsigned char *> framebuffer;
+	ofTexture tex;
+	unsigned char *pix;
+
+};
