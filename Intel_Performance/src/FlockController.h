a7c86ea13b96a0aff07b34b14dac8f9468163d34
Marek Bereza <bereza@gmail.com>
2012-07-09 16:25:14 +0100

    bum

--- Intel_Performance/src/FlockController.h
+++ Intel_Performance/src/FlockController.h
@@ -10,12 +10,30 @@ struct BoidParticle : public Particle<T> {
 	BoidParticle(const T& pos, float mass)
 		:Particle<T>(pos, mass)
 	{
+		Particle<T>::lifespan = FLT_MAX;
+
 	}
 	
+	void kill() {
+		Particle<T>::lifespan = Particle<T>::age;
+	}
+	
+
+	
 	void update(float dt) {
+
 		// is called after each integration step.
 	}
 };
+
+class Explosion {
+public:
+	ofVec3f pos;
+	void init(const ofVec3f &pos) {
+		this->pos = pos;
+	}
+};
+
 
 typedef BoidParticle<ofVec3f> BoidParticle3;
 typedef Particles<ofVec3f, BoidParticle<ofVec3f>, Spring<ofVec3f> >	BoidParticles3;
@@ -27,7 +45,7 @@ public:
 	void setup();
 	void setupGui();
 	void debugDraw();
-	void update();
+	void update(unsigned char *thresh);
 	void checkBounds();
 	
 	BoidParticles3 ps;
@@ -35,6 +53,7 @@ public:
 	float center_energy;
 	float sphere_size;
 	ofImage fluff;
+	vector<Explosion> explosions;
 	
 };
 
