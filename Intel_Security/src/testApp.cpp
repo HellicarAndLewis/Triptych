03807363f38a89490a939ebf5628dec6d49c3a8c
roxlu <diederick@apollomedia.nl>
2012-07-10 17:21:40 +0200

    Update readme

--- Intel_Security/src/testApp.cpp
+++ Intel_Security/src/testApp.cpp
@@ -13,16 +13,16 @@ void testApp::setup(){
 	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
 	ofEnableNormalizedTexCoords();
-	show_gui = true;
-	debug = true;
+	show_gui = false;
+	debug = false;
 	
 	app.setup();
-	
 	room.setup((float)ofGetWidth()/(float)ofGetHeight());
 	room.setupGui();
 	gui.loadFromXML();
 	gui.setAutoSave(true);
 	
+	float attack_col[3] = {0.5, 0.0, 0.3};
 	float flock_col[3] = {0.38,0.38,0.06};
 	flock_gui.addFloat("Flock radius SQ", app.flock.zone_radius_sq).setMin(0.0f).setMax(5.4).setColor(flock_col);
 	flock_gui.addFloat("Flock high threshold (align)", app.flock.high).setMin(0.0f).setMax(1.0f).setColor(flock_col);
@@ -32,11 +32,6 @@ void testApp::setup(){
 	flock_gui.addFloat("Flock attract energy", app.flock.attract_energy).setMin(0.0f).setMax(0.01f).setColor(flock_col);
 	flock_gui.addFloat("Flock center energy", settings.flocking_center_energy).setMin(0.0f).setMax(5.0f).setColor(flock_col);
 	flock_gui.addFloat("Flock sphere size", settings.flocking_sphere_size).setMin(3.0f).setMax(10.0f).setColor(flock_col);
-	//flock_gui.addBool("Flock apply perlin noise", settings.flocking_apply_perlin);//.setColor(0.4, 0.03, 0.1);
-	//flock_gui.addFloat("Flock perlin scale", settings.flocking_perlin_scale).setMin(0.0f).setMax(10.0f); //.setColor(0.4, 0.03, 0.1);
-	//flock_gui.addFloat("Flock perlin influence", settings.flocking_perlin_influence).setMin(0.0f).setMax(100.0f); //.setColor(0.4, 0.03, 0.1);
-	//flock_gui.addFloat("Boid trail duration (millis)", settings.boid_trail_duration_millis).setMin(0.0f).setMax(5000.0f);
-	//flock_gui.addFloat("Boid glow duration (ms)", settings.boid_glow_duration_millis).setMin(0.0f).setMax(5000.0f);
 	flock_gui.addFloat("Boid glow size", settings.boid_glow_size).setMin(0.0f).setMax(2.0f);
 	flock_gui.addFloat("Boid scale",settings.boid_scale).setMin(0.0f).setMax(1.0f);
 	flock_gui.addFloat("Boid trail width", settings.boid_trail_width).setMin(0.01f).setMax(2.1f);
@@ -51,16 +46,6 @@ void testApp::setup(){
 	flock_gui.addFloat("Boid attack delay max (ms)", settings.boid_attack_delay_max).setMin(0.0f).setMax(20000.0f);
 	flock_gui.addFloat("Boids: mix of normal in boid shader", settings.boid_shader_normal_mix).setMin(0.0f).setMax(1.0f);
 	flock_gui.addFloat("Boids: specular component (lower is more)", settings.boid_shader_specular).setMin(0.0f).setMax(10.0f);
-
-	/*
-	flock_gui.addFloat("Explosion random x velocity", settings.explosion_random_x_vel).setMin(0.0f).setMax(15.0f);
-	flock_gui.addFloat("Explosion random y velocity", settings.explosion_random_y_vel).setMin(0.0f).setMax(15.0f);
-	flock_gui.addFloat("Explosion min lifespan", settings.explosion_min_lifespan).setMin(0.0f).setMax(100.0f);
-	flock_gui.addFloat("Explosion max lifespan", settings.explosion_max_lifespan).setMin(0.0f).setMax(100.0f);
-	flock_gui.addFloat("Explosion perlin scale", settings.explosion_perlin_scale).setMin(0.0f).setMax(10.0f);
-	flock_gui.addFloat("Explosion perlin influence", settings.explosion_perlin_influence).setMin(0.0f).setMax(1.0f);
-	flock_gui.addInt("Explosion trail length", settings.explosion_trail_length).setMin(0).setMax(20);
-	*/
 	flock_gui.addBool("Record kinect", settings.must_record_kinect);
 	flock_gui.addBool("Draw boid glows", settings.boid_draw_glows);
 	flock_gui.addBool("Draw grid", settings.draw_axis);
@@ -69,19 +54,13 @@ void testApp::setup(){
 	flock_gui.addBool("DebugDraw attackers", settings.debugdraw_attackers);
 	flock_gui.addFloat("Kinect scale", settings.kinect_scale).setMin(0.0f).setMax(15.0f);
 	flock_gui.addButton<testApp>("Save kinect recording", 0, this);	
-	
-		
-	// Change number of boids.
-	flock_col[0] = 0.5f; flock_col[1] = 0.0f; flock_col[2] = 0.3f;
-	flock_gui.addFloat("Percentage of visible boids", settings.boids_percentage_visible).setMin(0.0f).setMax(1.0f).setColor(flock_col);
-	flock_gui.addFloat("Percentage of attack boids", settings.boids_percentage_attackers).setMin(0.0f).setMax(1.0f).setColor(flock_col);
-	flock_gui.addButton<testApp>("Update number of visible boids", 1, this).setColor(flock_col);
-
+	flock_gui.addFloat("Percentage of visible boids", settings.boids_percentage_visible).setMin(0.0f).setMax(1.0f).setColor(attack_col);
+	flock_gui.addFloat("Percentage of attack boids", settings.boids_percentage_attackers).setMin(0.0f).setMax(1.0f).setColor(attack_col);
+	flock_gui.addButton<testApp>("Update number of visible boids", 1, this).setColor(attack_col);
 	flock_gui.load(ofToDataPath("gui.bin",true));
-	
+
 	cam.setup(ofGetWidth(), ofGetHeight());
 	cam.translate(0,0,5);
-	//cam.orthoTopLeft(ofGetWidth(), ofGetHeight(), 0.0, 100.0f);
 	ax.setup(10);
 }
 
