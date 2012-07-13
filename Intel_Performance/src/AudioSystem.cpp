61cdead74aa5415514d640938396913b9825df4c
whg <wgallia@gmail.com>
2012-07-13 12:24:42 +0100

    Started the audio system

---
+++ Intel_Performance/src/AudioSystem.cpp
@@ -0,0 +1,9 @@
+//
+//  AudioSystem.cpp
+//  Performance
+//
+//  Created by Joel Lewis on 12/07/2012.
+//  Copyright (c) 2012 Hellicar & Lewis. All rights reserved.
+//
+
+#include <iostream>
---
+++ Intel_Performance/src/AudioSystem.h
@@ -0,0 +1,14 @@
+//
+//  AudioSystem.h
+//  Performance
+//
+//  Created by Joel Lewis on 12/07/2012.
+//  Copyright (c) 2012 Hellicar & Lewis. All rights reserved.
+//
+
+#ifndef Performance_AudioSystem_h
+#define Performance_AudioSystem_h
+
+
+
+#endif
