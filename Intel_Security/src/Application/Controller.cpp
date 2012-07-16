87d3cfb04742a63978d629687ff25b3d1119dace
roxlu <diederick@apollomedia.nl>
2012-07-16 12:41:13 +0200

    Cleaning up Controller.cpp

--- Intel_Security/src/Application/Controller.cpp
+++ Intel_Security/src/Application/Controller.cpp
@@ -98,61 +98,22 @@ void Controller::update() {
 				}
 
 				if(amode == 0) { 
-					// attract to point (user)
+					// attract
 					float len = dir.length();
-//					float F = 1.0f - (1.0/ls);
 					float F = 1.0f - (1.0/len);
-//					dir.normalize();
 					dir/=len;
 					dir *= F * settings.attract_to_user_energy;
 					b.addForce(dir);
 				}
 				else if(amode == 1) {
-					
-						float len = dir.length();
-//						float F = 1.0f/ls;	
-						float F = 1.0f/len;
-						dir /= len;
-//						dir.normalize();
-						dir *= F * settings.repel_from_user_energy;
-						dir *= -1;
-						b.addForce(dir);
-						
-					//}
-					//else {
-					//	b.attack_end = now-1;
-					//}
+					// repel				
+					float len = dir.length();
+					float F = 1.0f/len;
+					dir /= len;
+					dir *= F * settings.repel_from_user_energy;
+					dir *= -1;
+					b.addForce(dir);
 				}
-
-				
-				/*
-				// added a swithch between modes... attract/repulse
-				int amode = 1;
-				if(amode == 0) { // testing with modes.
-					// attract to point.
-					if(ls > 0.3f) {
-						float F = 1.0f - (1.0/ls);
-						dir.normalize();
-						dir *= F * settings.boid_attack_energy;
-						b.addForce(dir);
-					}
-					else {
-						b.attack_end = now-1;
-					}
-				}
-				else if(amode == 1) {
-					if(ls > settings.attract_to_user_radius) {
-						float F = 1.0f/ls;	
-						dir.normalize();
-						dir *= F * settings.attract_to_user_energy;
-						dir *= -1;
-						b.addForce(dir);
-					}
-					else {
-						b.attack_end = now-1;
-					}
-				}
-				*/
 			}
 		}
 		else if(b.mode == B_MODE_ATTACK && b.attack_end < now) {
